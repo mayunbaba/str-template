6d2e461f986281bbb18c0995f47cc2a569c8dddf
刘伟华 <liuweihua001@chinasofti.com>
2023-03-22 12:57:12 +0800

    后台配置

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,8 +1,42 @@
+import Logo from "./extensions/logo.png";
+
 const config = {
-  locales: [
-    'zh-Hans',
-    'en',
-  ],
+  // Replace the Strapi logo in auth (login) views
+  // auth: {
+  //   logo: Logo,
+  // },
+  // Replace the favicon
+  head: {
+    favicon: Logo,
+  },
+  // Replace the Strapi logo in the main navigation
+  // menu: {
+  //   logo: Logo,
+  // },
+  locales: ["zh-Hans", "en"],
+  // Override or extend the theme
+  theme: {
+    // overwrite light theme properties
+    light: {
+      // colors: {
+      //   primary100: '#f6ecfc',
+      //   primary200: '#e0c1f4',
+      //   primary500: '#ac73e6',
+      //   primary600: '#9736e8',
+      //   primary700: '#8312d1',
+      //   danger700: '#b72b1a'
+      // },
+    },
+
+    // overwrite dark theme properties
+    dark: {
+       // ...
+    }
+  },
+  // Disable video tutorials
+  tutorials: true,
+  // Disable notifications about new Strapi releases
+  notifications: { releases: false },
 };
 
 const bootstrap = (app) => {
