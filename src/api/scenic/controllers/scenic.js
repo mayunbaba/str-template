ae4ac46054fd86be860b5f0e10fdb7698f9d831a
liuweihua <18538302307@163.com>
2023-03-26 10:10:43 +0800

    自动获取经纬度

--- src/api/scenic/controllers/scenic.js
+++ src/api/scenic/controllers/scenic.js
@@ -3,7 +3,42 @@
 /**
  * scenic controller
  */
+const baidu = require('../../../utils/baidu');
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::scenic.scenic');
+module.exports = createCoreController('api::scenic.scenic', (strapi) => ({
+  // 重写find方法，修改出参
+  // async find(ctx) {
+  //   let { data, meta } = await super.find(ctx);
+  //   data.forEach((item) => {
+  //     Object.assign(item, item.attributes);
+  //     delete item.attributes;
+  //   });
+  //   return { data, meta };
+  // },
+  // 重写create方法，修改入参
+  async create(ctx) {
+    const res = await baidu.geocoding(ctx.request.body.data.city, ctx.request.body.data.name);
+    if (res.longitude) {
+      ctx.request.body.data.longitude = res.longitude;
+      ctx.request.body.data.latitude = res.latitude;
+      return await super.create(ctx);
+    } else {
+      ctx.status = 400;
+      return res;
+    }
+  },
+  // 重写update方法，修改入参
+  async update(ctx) {
+    const res = await baidu.geocoding(ctx.request.body.data.city, ctx.request.body.data.name);
+    if (res.longitude) {
+      ctx.request.body.data.longitude = res.longitude;
+      ctx.request.body.data.latitude = res.latitude;
+      return await super.update(ctx);
+    } else {
+      ctx.status = 400;
+      return res;
+    }
+  },
+}));
